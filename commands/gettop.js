fe0b56f6aa91eb65c4ed143b19380b43666128fe
Hyla Aster <sethvale@mail.com>
2017-12-13 02:15:27 -0800

    gettop now works

--- commands/gettop.js
+++ commands/gettop.js
@@ -3,17 +3,19 @@ exports.run = (client, message, args) => {
     let secondrating = 0;
     let topdog = "";
     let runnerup = "";
-    for (var key in client.records.keys()){
-        let rating = client.records.get(key);
+    console.log("--");
+    for (var key of client.records.keyArray()){
+        console.log(key);
+        let rating = Number(client.records.get(key));
         if (rating > maxrating){
             secondrating = maxrating;
             runnerup = topdog;
             maxrating = rating;
-            topdog = key;
+            topdog = client.users.get(key);
         } else if (rating > secondrating){
             secondrating = rating;
-            runnerup = key;
-        }
+            runnerup = client.users.get(key);
+        }    
     }
     message.channel.send(`${topdog}'s rating is ${maxrating} and ${runnerup}'s is ${secondrating}`);
 }
