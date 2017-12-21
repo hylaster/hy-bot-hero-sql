f5bc62d46b8d54bee11d7dc887ff552c9a35fa37
seth vale <sethvale@mail.com>
2017-12-20 19:38:55 -0500

    update gettop reporting

--- commands/gettop.js
+++ commands/gettop.js
@@ -1,10 +1,11 @@
 exports.run = (client, message, args, pool) => {
     console.log("--");
 
-    pool.query("SELECT userid, rating FROM UserByServer ORDER BY rating DESC WHERE server = " + message.guild.id + " LIMIT 2;", function(err, results){
+    pool.query("SELECT userid, rating FROM UserByServer WHERE server = " + message.guild.id + " ORDER BY rating DESC LIMIT 2;", function(err, results){
         if (err) console.log(err);
         else {
             if (!results[1]){
+                message.channel.send(`Two users do not exist.`);
                 console.log("Two users do not exist.");
             } else {
                 console.log("Successfully found top two players.");
