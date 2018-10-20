70432e7a33e73d830dadef729ca5bbb816ac0b2c
andrewkolos <andrewrkolos@gmail.com>
2018-10-19 22:16:21 -0500

    begin TypeScript migration

---
+++ index.ts
@@ -0,0 +1,74 @@
+import Discord from "discord.js";
+const client = new Discord.Client();
+const config = require("./config.json");
+import express from "express";
+import http from "http";
+const mysql = require("mysql");
+
+import Command from './commands/command';
+
+const port = process.env.PORT || 5000;
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b2bd33981e831f',
+    password: '5ee7aa57',
+    database: 'heroku_73d18bb7c7b731d'
+});
+
+/*
+pool.query("ALTER TABLE `MatchByServer` ADD CONSTRAINT `MatchByServer_fk2` FOREIGN KEY (`server`) REFERENCES `UserByServer`(`server`);", function(err, results){
+        if (err) console.log(err);
+        else console.log(results);
+});
+*/
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (_request, response) => {
+    response.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}.`);
+});
+
+client.on("ready", () => {
+    console.log("Ready.");
+});
+
+
+const handleMessage = (message: Discord.Message) => {
+
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const words: string[] = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
+    if (words == null || words.length == 0) return;
+
+    const commandName = words[0] == null ? null : words[0].toLowerCase();
+    const args = words.splice(1);
+
+    if (commandName == null) return;
+    if (commandName.charAt(0) === ".") {
+        message.channel.send("Potential attempt to access outside of commands. Ignoring command.");
+    } else {
+        const command: Command = require(`./commands/${commandName}.js`);
+        if (command != null) {
+            command(client, message, args, pool);
+        }
+    }
+}
+
+client.on("message", handleMessage);
+
+client.login(config.token);
+
+setInterval(() => {
+    http.get('https://hy-bot-hero-sql.herokuapp.com');
+}, 900000);
