9f9c1f64943483f0d889f6af73bb0d0979546c74
andrewkolos <andrewrkolos@gmail.com>
2018-11-01 22:46:51 -0500

    add command location functionality

---
+++ src/command/commands/index.ts
@@ -0,0 +1,7 @@
+export { GetRating } from './getrating';
+export { GetTop } from './gettop';
+export { Ping } from './ping';
+export { Record } from './record';
+export { ResetRank } from './resetrank';
+export { Roll } from './roll';
+export { Timer } from './timer';
--- src/command/locator.ts
+++ src/command/locator.ts
@@ -1,12 +1,23 @@
+import * as Commands from './commands';
+import { CommandName } from './names';
 import { Command } from './command';
 
-const mappings: [number, Command][];
+const commandMap: Map<CommandName, Command> = new Map([
+  [CommandName.GetRating, Commands.GetRating],
+  [CommandName.GetTop, Commands.GetTop],
+  [CommandName.Ping, Commands.Ping],
+  [CommandName.Record, Commands.Record],
+  [CommandName.ResetRank, Commands.ResetRank],
+  [CommandName.Roll, Commands.Roll],
+  [CommandName.Timer, Commands.Timer]
+]);
 
-export class CommandLocator {
+export namespace CommandLocator {
+  /**
+   * fuk
+   * @param name The name of the command, as it would be entered by a user.
+   */
+  export const getCommand = (name: string) => commandMap.get(name as CommandName);
 
-  private static nameMap = new Map<string, Command>(map)
-
-  static getCommand(name: string) {
-
-  }
+  export const commandWithNameExists = (name: string) => commandMap.has(name as CommandName);
 }
