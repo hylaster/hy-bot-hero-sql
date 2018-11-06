6fb6b6f7913149d9b3c0b6a5bd60da6671e973be
andrewkolos <andrewrkolos@gmail.com>
2018-11-05 20:01:04 -0600

    move mysql pool locator to entry point
    Configuration constant should not be part of the library code.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import http from 'http';
-import { MySqlPoolLocator } from './data/sql/mysql-pool-locator';
-import { MySqlDataService } from './data/mysql-data-service';
+import { MySqlDataService } from './data/sql/mysql-data-service';
 import { HyBot, ConnectResponse } from './hybot';
 import { HyBotMySqlConfig } from './config/hybot-mysql-config';
+import mysql from 'mysql';
 
 const config: HyBotMySqlConfig = require('../config.json');
 
-const pool = MySqlPoolLocator.getPool();
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'hy-bot.cbce7r2dyrtw.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'root',
+  password: 'superasbestosman34'
+});
+
 const dataService = new MySqlDataService(pool, config.sql.userTableName, config.sql.matchTableName);
 const bot = new HyBot(config, dataService);
 bot.connect().then((resp: ConnectResponse) => {
