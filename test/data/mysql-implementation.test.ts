6c4049d7738309c0a601634457bb42af20061ff1
andrewkolos <andrewrkolos@gmail.com>
2019-04-13 09:01:33 -0500

    update data-service import

--- test/data/mysql-implementation.test.ts
+++ test/data/mysql-implementation.test.ts
@@ -1,4 +1,4 @@
-import { DataServiceTester } from './data-service';
+import { DataServiceTester } from './data-service-tester';
 
 import mysql from 'mysql';
 import { MySqlDataService } from '../../src/data/sql/mysql-implementation/mysql-data-service';
