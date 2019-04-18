0d402fadbdfc226d4baf9b78bd9d39c33cc0c1c1
andrewkolos <andrewrkolos@gmail.com>
2019-04-18 12:07:07 -0500

    change config to use environment variables

---
+++ hy-bot-config.ts
@@ -0,0 +1,31 @@
+import { HyBotMySqlConfig } from 'src/config/hybot-mysql-config';
+
+const host = process.env.DATABASE_HOST;
+const port = process.env.DATABASE_PORT as number | undefined;
+const database = process.env.DATABASE_DB_NAME;
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
+
+[host, port, database, user, password].forEach((item) => {
+  if (item == null) {
+    throw Error('Database connection information is missing.');
+  }
+});
+
+export const config: HyBotMySqlConfig = {
+  token: 'NTAzNzU4NTM4NDE4NTUyODMy.DsAZgA.DU9XxTFbv_qAinykdbR_YIN9IHY',
+  prefix: '$',
+  owners: ['368485403340046336'],
+  'clientConfig': {},
+  'sql': {
+    'connectionInfo': {
+      host: host!,
+      port: port!,
+      database: database!,
+      user: user!,
+      password: password!
+    },
+    'userTableName': 'user_by_server_test',
+    'matchTableName': 'match_by_server_test'
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import http from 'http';
 import { MySqlDataService } from './data/sql/mysql-implementation/mysql-data-service';
 import { HyBot } from './hybot';
-import { HyBotMySqlConfig } from './config/hybot-mysql-config';
 import mysql from 'mysql';
 import { HyBotConfig } from './config/hybot-config';
 import { DataService } from './data/data-service';
@@ -10,35 +9,25 @@ import { Help } from './command/commands/help';
 
 // Spins up an implementation of Hybot.
 
-const config: HyBotMySqlConfig = require('../config.json');
+import { config } from '../hy-bot-config';
 
 async function start() {
-  const { user, password } = getCredentials();
 
-  if (user == null || password == null) {
-    throw Error('Database user and/or password environment variables are missing.');
-  }
+  const { host, port, database, user, password } = config.sql.connectionInfo;
 
   const pool = mysql.createPool({
     connectionLimit: 10,
     connectTimeout: 60 * 60 * 1000,
     timeout: 60 * 60 * 1000,
-    host: 'hybot.cbce7r2dyrtw.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    database: 'hybot',
+    host,
+    port,
+    database,
     user,
     password
   });
 
   const dataService = await MySqlDataService.createService(pool, config.sql.userTableName, config.sql.matchTableName, true);
   startBot(config, dataService);
-}
-
-function getCredentials(): { user?: string, password?: string } {
-  const user = process.env.DATABASE_USER;
-  const password = process.env.DATABASE_PASSWORD;
-
-  return { user, password };
 }
 
 async function startBot(config: HyBotConfig, dataService: DataService) {
